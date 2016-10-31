3da7f6416b8e8c67131bbc7c657697de22303a14
Pavel Cherednichenko <p.cherednichenko@topface.com>
2016-10-31 17:41:55 +0300

    убрано комментирование

--- test_two.php
+++ test_two.php
@@ -5,6 +5,6 @@ ini_set('memory_limit', '10G');
 $Geo = new GeoTwo();
 $Geo->clearTable("info");
 $Geo->ipSort();
-//$Geo->groupHundredElements();
-//$Geo->writeInBd();
+$Geo->groupHundredElements();
+$Geo->writeInBd();
 ?>
